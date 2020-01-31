5b9a95cd2416fadc03013a6fcfce700dbe7dfb01
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2020-01-31 19:18:17 +0100

    Update UpdateIncident.js

--- UpdateIncident.js
+++ UpdateIncident.js
@@ -1,29 +1,29 @@
 var request = require("request");
 const conf = {
-    url: process.env.url,
-    key: process.env.key
-  }
+  url: process.env.URL,
+  key: process.env.KEY
+};
 module.exports = {
-//Update(37, 4, "Fixed")
-Update : function (id, status, message) {
+  //Update(37, 4, "Fixed")
+  Update: function(id, status, message) {
     var options = {
-        method: 'POST',
-        headers: {
-            'X-Cachet-Token': conf.KEY
-        },
-        url: conf.URL + 'api/v1/incidents/' + id + '/updates',
-        body: {
-            status: status,
-            message: message,
-            user_id: 1
-        },
-        json: true
+      method: "POST",
+      headers: {
+        "X-Cachet-Token": conf.key
+      },
+      url: conf.url + "/api/v1/incidents/" + id + "/updates",
+      body: {
+        status: status,
+        message: message,
+        user_id: 1
+      },
+      json: true
     };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+    request(options, function(error, response, body) {
+      if (error) console.log(error);
 
-        console.log(body);
+      console.log(body);
     });
-}
-}
+  }
+};
